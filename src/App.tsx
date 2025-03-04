d1f7f4a769585c58fb4a7462e33a462223b532e0
Willian Brusch <willian.bruschh@gmail.com>
2025-03-04 18:23:45 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import Header from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="wrapper">
+        <h1>Teste</h1>
+        <p>Some content and stuff</p>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
