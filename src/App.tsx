5bf22d0f8d6be54248db83881588e79e04848129
Willian Brusch <willian.bruschh@gmail.com>
2025-03-06 23:04:38 -0300

    remove some code and libs

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,12 @@ function App() {
     <>
       <Header />
       <main className="wrapper">
+        <div></div>
         <h1>Oi! Meu nome é Willian!</h1>
         <h2>
           Sou um desenvolvedor de software com foco em desensolver soluções!
         </h2>
-        <p>Some content and stuff</p>
       </main>
-
       <footer></footer>
     </>
   );
